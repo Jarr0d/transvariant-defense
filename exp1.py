bf49ed42efbfa5896a5a8b9fc123cbdce5f295d6
Jarr0d <wangzhewz@buaa.edu.cn>
2020-01-05 02:34:09 -0700

    update TRADES

--- exp1.py
+++ exp1.py
@@ -12,11 +12,12 @@ data2 = 7
 eps = 0.2
 lmd_1 = 0.3
 lmd_2 = 0.02
+lmd_3 = 1.0
 lr = 0.01
 batch_size = 128
 img_size = 28
 crop_size = 26
-epochs = 10
+epochs = 5
 
 # plot data correlation
 mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
@@ -24,10 +25,8 @@ digit5 = mnist.train.images[mnist.train.labels == data1]    # num: 4987
 digit7 = mnist.train.images[mnist.train.labels == data2]    # num: 5715
 mean5 = np.mean(digit5, 0)
 mean7 = np.mean(digit7, 0)
-# mean5[mean5<0.2] = 0
-# mean7[mean7<0.2] = 0
-corr = np.abs(mean7.reshape(img_size, img_size) - mean5.reshape(img_size, img_size)) / 2
-corr[corr < eps] = 0
+corr = (mean7.reshape(img_size, img_size) - mean5.reshape(img_size, img_size)) / 2
+corr[abs(corr) < eps] = 0
 plt.imshow(corr, cmap='seismic')
 plt.show()
 
@@ -38,7 +37,7 @@ y = tf.placeholder(tf.float32, shape=(None))
 # w = np.random.randn(img_size**2)
 # w0 = w / np.linalg.norm(w, ord=2)
 # w = tf.Variable(np.asarray(w0, dtype='float32'), name="weights")
-
+#
 # pred = tf.reduce_sum(w*x, 1)
 # err = tf.clip_by_value(tf.ones_like(pred)-y*pred, 0, 20*batch_size)
 # acc = tf.reduce_mean(tf.cast(tf.equal(y,tf.sign(pred)),tf.float32))
@@ -48,32 +47,43 @@ y = tf.placeholder(tf.float32, shape=(None))
 # w = np.random.randn(img_size**2)
 # w0 = w / np.linalg.norm(w, ord=2)
 # w = tf.Variable(np.asarray(w0, dtype='float32'), name="weights")
-
+#
 # pred = tf.reduce_sum(w*x, 1)
 # err = tf.clip_by_value(tf.ones_like(pred) - y*pred + eps*tf.norm(w, ord=1), 0, 20*batch_size)
 # acc = tf.reduce_mean(tf.cast(tf.equal(y,tf.sign(pred)),tf.float32))
-# loss = tf.reduce_mean(err) + lmd_1/2*tf.norm(w, ord=2)
+# loss = tf.reduce_mean(err) + lmd_2/2*tf.norm(w, ord=2)
 
-# Ours
-w = np.random.randn(crop_size**2)
+# TRADES adv. training
+w = np.random.randn(img_size**2)
 w0 = w / np.linalg.norm(w, ord=2)
 w = tf.Variable(np.asarray(w0, dtype='float32'), name="weights")
 
-loc = [13, 14, 15]    #np.arange(10,18,1)
-x_img = tf.reshape(x, (-1, img_size, img_size))
-pred, err = [], []
-for i in loc:
-    for j in loc:
-        x_ij = tf.reshape(x_img[:, i-crop_size//2:i+crop_size//2, j-crop_size//2:j+crop_size//2],
-                          (-1, crop_size*crop_size))
-        pred_ij = tf.reduce_sum(w*x_ij, 1)
-        err_ij = tf.clip_by_value(tf.ones_like(pred_ij) - y*pred_ij + eps*tf.norm(w, ord=1), 0, crop_size*batch_size)
-        pred += [pred_ij]
-        err += [err_ij]
-err = tf.reduce_mean(err)
-pred = tf.reduce_mean(pred, 0)
+pred = tf.reduce_sum(w*x, 1)
+err = tf.clip_by_value(tf.ones_like(pred) - y*pred, 0, 20*batch_size)
+err_reg = tf.clip_by_value(tf.ones_like(pred) - pred*pred + tf.abs(pred)*eps*tf.norm(w, ord=1), 0, 20*batch_size)
 acc = tf.reduce_mean(tf.cast(tf.equal(y,tf.sign(pred)),tf.float32))
-loss = tf.reduce_mean(err) + lmd_2/2*tf.norm(w, ord=2)
+loss = tf.reduce_mean(err) + lmd_2/2*tf.norm(w, ord=2) + lmd_3*tf.reduce_mean(err_reg)
+
+# Ours
+# w = np.random.randn(crop_size**2)
+# w0 = w / np.linalg.norm(w, ord=2)
+# w = tf.Variable(np.asarray(w0, dtype='float32'), name="weights")
+#
+# loc = [13, 14, 15]    #np.arange(10,18,1)
+# x_img = tf.reshape(x, (-1, img_size, img_size))
+# pred, err = [], []
+# for i in loc:
+#     for j in loc:
+#         x_ij = tf.reshape(x_img[:, i-crop_size//2:i+crop_size//2, j-crop_size//2:j+crop_size//2],
+#                           (-1, crop_size*crop_size))
+#         pred_ij = tf.reduce_sum(w*x_ij, 1)
+#         err_ij = tf.clip_by_value(tf.ones_like(pred_ij) - y*pred_ij + eps*tf.norm(w, ord=1), 0, crop_size*batch_size)
+#         pred += [pred_ij]
+#         err += [err_ij]
+# err = tf.reduce_mean(err)
+# pred = tf.reduce_mean(pred, 0)
+# acc = tf.reduce_mean(tf.cast(tf.equal(y,tf.sign(pred)),tf.float32))
+# loss = tf.reduce_mean(err) + lmd_2/2*tf.norm(w, ord=2)
 
 # training setting
 train_op = tf.train.GradientDescentOptimizer(lr).minimize(loss)
@@ -118,27 +128,82 @@ print('test acc: {}'.format(test_acc))
 
 # weight visualization
 weight = sess.run(w)
+weight = weight / np.linalg.norm(weight, ord=2)
 size = int(np.sqrt(weight.size))
 weight = weight.reshape(size, size)
 # interplote to img_size*img_size
 weight = np.array(Image.fromarray(weight).resize((img_size, img_size)))
+weight[abs(weight) < 0.01] = 0
 plt.imshow(weight, cmap='seismic')
 plt.show()
 
 # weight correlation vs. #feature
-corr = np.abs(mean7.reshape(img_size, img_size) - mean5.reshape(img_size, img_size))/2
-plt.semilogx(np.sort(corr.flatten())[::-1],'k--')
-idx = np.argsort(corr.flatten())[::-1]
+corr = (mean7.reshape(img_size, img_size) - mean5.reshape(img_size, img_size))/2
+
+# for standard training
+plt.semilogx(np.sort(abs(corr.flatten()))[::-1],'k--')
+idx = np.argsort(abs(corr.flatten()))[::-1]
 plt.semilogx(abs(weight.flatten())[idx],'r')
+plt.show()
+
+# for adv. training
+w_at = corr.copy()
+w_at[abs(w_at) < eps]=0
+w_at[w_at > 0] -= eps
+w_at[w_at < 0] += eps
+w_at_abs = np.sort(abs(w_at))[::-1]/2.
+plt.semilogx(np.sort(abs(corr))[::-1], 'k--')
+plt.semilogx(w_at_abs, 'r')
 plt.show()
 
 # np.savetxt('madry.txt',abs(sess.run(w).flatten())[idx])
 
+# acc. vs. #feature
+digit5 = mnist.train.images[mnist.train.labels == data1]
+digit7 = mnist.train.images[mnist.train.labels == data2]
+y_5 = np.array([-1]*len(digit5))
+y_7 = np.array([1]*len(digit7))
 
-print(np.linalg.norm(sess.run(w)))
+mean5 = np.mean(digit5, 0)
+mean7 = np.mean(digit7, 0)
+corr = (mean7.reshape(img_size, img_size) - mean5.reshape(img_size, img_size))/2
+corr = corr.flatten()
+idx = np.argsort(abs(corr))[::-1]
 
+# for standard training
+weight = sess.run(w)
+w_ = np.zeros_like(corr)
+clean_acc = []
+for p in range(img_size*img_size):
+    w_[idx[p]] = weight[idx[p]]
+    acc_5 = y_5 * np.matmul(digit5, w_)
+    acc_7 = y_7 * np.matmul(digit7, w_)
+    # acc_ = (sum(acc_5>0) + sum(acc_7>=0)) / (len(digit5) + len(digit7))
+    if sum(acc_7 > 0) == 0:
+        acc_ = sum(acc_5 > 0) / len(digit5)
+    else:
+        acc_ = (sum(acc_5 > 0) + sum(acc_7 > 0)) / (len(digit5) + len(digit7))
+    print('acc: {}'.format(acc_))
+    clean_acc.append(acc_)
+plt.semilogx(clean_acc,'b--')
+plt.show()
 
+np.save('clean_acc_train', clean_acc)
 
+# for adv. training
+w_ = np.zeros_like(corr)
+robust_acc = []
+for p in range(img_size*img_size):
+    w_[idx[p]] = corr[idx[p]]
+    acc_5 = y_5 * np.matmul(digit5, w_) - eps*np.linalg.norm(w_, ord=1)
+    acc_7 = y_7 * np.matmul(digit7, w_) - eps*np.linalg.norm(w_, ord=1)
+    if sum(acc_7 > 0) == 0:
+        acc_ = sum(acc_5 > 0) / len(digit5)
+    else:
+        acc_ = (sum(acc_5 > 0) + sum(acc_7 > 0)) / (len(digit5) + len(digit7))
+    print('acc: {}'.format(acc_))
+    robust_acc.append(acc_)
+plt.semilogx(robust_acc,'r--')
+plt.show()
 
-
-
+np.save('robust_acc_train', robust_acc)
